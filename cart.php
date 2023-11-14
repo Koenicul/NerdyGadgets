df257cc3afd98a32d405e7a13c9034b2b25827e4
Koenicul <koenvaartjes2005@gmail.com>
2023-11-14 21:35:12 +0100

    Added remove from cart

--- cart.php
+++ cart.php
@@ -3,6 +3,12 @@
 include __DIR__ . "/header.php";
 include "cartfuncties.php";
 
+if (isset($_GET["id"])) {
+    $cart = getCart();
+    unset($cart[$_GET["id"]]);
+    saveCart($cart);
+}
+
 if (isset($_POST["quantity"]) && isset($_POST["id"])) {
     $cart = getCart();
     $cart[$_POST["id"]] = $_POST["quantity"];
@@ -11,75 +17,76 @@ if (isset($_POST["quantity"]) && isset($_POST["id"])) {
     saveCart($cart);
 }
 ?>
-<div>
+<div class="p-2">
     <h1>Inhoud Winkelwagen</h1>
-    <div>
-        <?php
-        if (isset($_SESSION['cart'])) {
-        $cart = getCart();
-        $price = 0;
-        $korting = 5;
-        
-        foreach ($cart as $id => $quantity) {
-            $StockItem = getStockItem($id, $databaseConnection);
-            $StockItemImage = getStockItemImage($id, $databaseConnection);
-
-            $price += round($StockItem["SellPrice"], 2) * $quantity;
-            ?>
+    <div class="row">
+        <div class="col-8">
+            <?php
+            if (count($_SESSION['cart']) > 0) {
+            $cart = getCart();
+            $price = 0;
+            $korting = 5;
             
-            <!-- <a class="ListItem" href='view.php?id=<?php print $StockItem['StockItemID']; ?>'> -->
-            <div id="ProductFrame">
-                <?php
-                if (isset($StockItemImage[0]['ImagePath'])) { ?>
-                    <div class="ImgFrame"
-                        style="background-image: url('<?php print "Public/StockItemIMG/" . $StockItemImage[0]['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
-                <?php } else if (isset($StockItem['BackupImagePath'])) { ?>
-                    <div class="ImgFrame"
-                        style="background-image: url('<?php print "Public/StockGroupIMG/" . $StockItem['BackupImagePath'] ?>'); background-size: cover;"></div>
-                <?php }
+            foreach ($cart as $id => $quantity) {
+                $StockItem = getStockItem($id, $databaseConnection);
+                $StockItemImage = getStockItemImage($id, $databaseConnection);
+
+                $price += round($StockItem["SellPrice"], 2) * $quantity;
                 ?>
+                
+                <!-- <a class="ListItem" href='view.php?id=<?php print $StockItem['StockItemID']; ?>'> -->
+                <div id="ProductFrame">
+                    <?php
+                    if (isset($StockItemImage[0]['ImagePath'])) { ?>
+                        <div class="ImgFrame"
+                            style="background-image: url('<?php print "Public/StockItemIMG/" . $StockItemImage[0]['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
+                    <?php } else if (isset($StockItem['BackupImagePath'])) { ?>
+                        <div class="ImgFrame"
+                            style="background-image: url('<?php print "Public/StockGroupIMG/" . $StockItem['BackupImagePath'] ?>'); background-size: cover;"></div>
+                    <?php }
+                    ?>
 
-                <div id="StockItemFrameRight">
-                    <div class="CenterPriceLeftChild">
-                        <h1 class="StockItemPriceText"><?php print sprintf("€ %.2f", round($StockItem["SellPrice"], 2) * $quantity); ?></h1>
-                        <h6>Inclusief BTW </h6>
+                    <div id="StockItemFrameRight">
+                        <div class="CenterPriceLeftChild">
+                            <h1 class="StockItemPriceText"><?php print sprintf("€ %.2f", round($StockItem["SellPrice"], 2) * $quantity); ?></h1>
+                            <h6>Inclusief BTW </h6>
+                        </div>
                     </div>
-                </div>
-            
-                <h1 class="StockItemID">Artikelnummer: <?php print $StockItem["StockItemID"]; ?></h1>
-                <p class="StockItemName"><?php print $StockItem["StockItemName"]; ?></p>
-            
+                
+                    <h1 class="StockItemID">Artikelnummer: <?php print $StockItem["StockItemID"]; ?></h1>
+                    <p class="StockItemName"><?php print $StockItem["StockItemName"]; ?></p>
+                
 
 
-                <p class="StockItemComments"><?php print $StockItem["MarketingComments"]; ?></p>
-                <div>
+                    <p class="StockItemComments"><?php print $StockItem["MarketingComments"]; ?></p>
+                    
+                    <!-- Wijzigen van hoeveelheid product -->
                     <form action="cart.php" method="post">
-                    <input hidden name="id" value=<?php print $StockItem["StockItemID"] ?>>
-                    <input style="width: fit-content" name="quantity" type="number" min="1" value=<?php print $quantity; ?>>
-                    <input style="width: fit-content" type="submit" value="Test">
-
+                        <input hidden name="id" value=<?php print $StockItem["StockItemID"] ?>>
+                        <input style="width: fit-content" name="quantity" type="number" min="1" value=<?php print $quantity; ?>>
+                        <input style="width: fit-content" type="submit" value="Test">
                     </form>
 
+                    <a href="cart.php?id=<?php print $id ?>" class="HrefDecoration"><i class="fa fa-solid fa-trash"></i></a>
                 </div>
-            </div>
-            <!-- </a> -->
-        <?php } ?>
-    </div>
-    <!-- Samenvatting van winkelmandje -->
-    <div>
-        <p>Overzicht</p>
-        <p>Artikelen : € <?php print $price ?></p>
-        <?php if ($korting != 0) { ?>
-            <p>Korting : € <?php print $korting ?></p>
-        <?php } ?>
-        <p>------------</p>
-        <p>Totaal : € <?php print $price - $korting; ?></p>
-    </div>
-    <?php
+                <!-- </a> -->
+            <?php } ?>
+        </div>
+        <!-- Samenvatting van winkelmandje -->
+        <div class="col-2">
+            <p><h3>Overzicht</h3></p>
+            <p>Artikelen : € <?php print $price ?></p>
+            <?php if ($korting != 0) { ?>
+                <p>Korting : € <?php print $korting ?></p>
+            <?php } ?>
+            <p>------------</p>
+            <p>Totaal : € <?php print $price - $korting; ?></p>
+        </div>
+        <?php
 
-    } else { ?>
-        <p>Winkelwagen is leeg</p>
-    <?php } ?>
+        } else { ?>
+            <p>Winkelwagen is leeg</p>
+        <?php } ?>
+    </div>
 </div>
-
 <?php include __DIR__ . "/footer.php"; ?>
