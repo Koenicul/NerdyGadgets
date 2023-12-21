b67d29498f4afd4bf9831d0e86ec5053d3cfcc5b
Sander <71982631+Sander090609@users.noreply.github.com>
2023-12-21 10:07:51 +0100

    Add files via upload

--- supportsubmit.php
+++ supportsubmit.php
@@ -3,23 +3,17 @@
 include __DIR__ . "/header.php";
 
 $send = FALSE;
-
-$name = $_POST['emailName'];
-$email = $_POST['emailFrom'];
-$topic = $_POST['emailTopic'];
-$description = $_POST['emailDescription'];
-
-$email_to = 'joshuadh@live.nl';
-$email_subject = ("New Support Ticket: ".$topic);
-$email_body = ("A new support ticket has been filled out by ".$name."\n. Here is the message:\n ".$description);
-
-$email_headers = "From: ".$email."\r\n";
+if (isset($_POST['emailName'])) {
+    $name = $_POST['emailName'];
+    $email = $_POST['emailFrom'];
+    $topic = $_POST['emailTopic'];
+    $description = $_POST['emailDescription'];
+}
 
 
 if (isset($_POST['emailName']) &&  $_POST['emailFrom'] && $_POST['emailTopic'] && $_POST['emailDescription']) {
     $send = TRUE;
     postTicket($databaseConnection, $email, $topic, $description, $name);
-    print("$email $topic $description $name");
 }
 
 //mail($email_to, $email_subject, $email_body, $email_headers);
@@ -39,7 +33,9 @@ if (isset($_POST['emailName']) &&  $_POST['emailFrom'] && $_POST['emailTopic'] &
     <p>Bedankt voor uw vraag. We gaan ermee bezig!</p>
     <?php
     if ($send) {
-        echo("Verzonden van: $email | Verzonden naar: $email_to | Onderwerp: $email_subject | Mail: $email_body | ");
+        echo("Het antwoord op uw vraag zal zo snel mogelijk behandeld worden. Het antwoord zal gestuurd worden naar $email.");
+    }else{
+        echo("Er is geen vraag gevonden.");
     }
     ?>
 </div>
